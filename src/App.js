b16a8fea3bfd13476af8e6c9b4a6d53284b07131
Elly Lastname <ellyy.yyu@gmail.com>
2021-05-08 15:32:05 -0400

    sus

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import './App.css';
+import Signup from "./components/pages/Signup"
+
+
+export default function App() {
+  return (
+  <>
+   <Router> 
+     <Switch>  
+      
+     <Route path = '/signup' exact component = {Signup} />
+     </Switch>  
+  
+   </Router>
+     
+
+   </>
+  );
+}
+
+
+
+
+
+
+
+
+
