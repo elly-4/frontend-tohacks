4acc8efdb9ec09fe6a7d9e1259f6d74347707704
Green-Robot-Dev-Studios <nficara01@gmail.com>
2021-05-09 09:11:52 -0400

    Prettier and Backend

--- src/App.js
+++ src/App.js
@@ -1,43 +1,23 @@
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 import Navbar from "./components/Navbar";
-import Signup from "./components/pages/Signup"
-import PickRecipes from "./components/pages/PickRecipes"
-import Home from "./components/pages/Home"
-import BrowseMeals from "./components/pages/BrowseMeals"
-
+import Signup from "./components/pages/Signup";
+import PickRecipes from "./components/pages/PickRecipes";
+import Home from "./components/pages/Home";
+import BrowseMeals from "./components/pages/BrowseMeals";
 
 export default function App() {
   return (
-  <>
-   <Router> 
-   <Navbar />
-     <Switch>  
-      
-     <Route path = '/signup' exact component = {Signup} />
-     <Route path = '/browsemeals' exact component = {BrowseMeals} />
-     <Route path = '/pickrecipes' exact component = {PickRecipes}/>
-     <Route path = '/' exact component = {Home}/>
-     </Switch>  
-  
-   </Router>
-     
-
-   </>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/signup" exact component={Signup} />
+          <Route path="/browsemeals" exact component={BrowseMeals} />
+          <Route path="/pickrecipes" exact component={PickRecipes} />
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </Router>
+    </>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,71 +1,56 @@
-import React from 'react';
+import React from "react";
 import "../../App.css";
-import './Home.css';
-import Card from 'react-bootstrap/Card';
-import CardColumns from 'react-bootstrap/CardColumns';
+import "./Home.css";
+import Card from "react-bootstrap/Card";
+import CardColumns from "react-bootstrap/CardColumns";
 
-function Home () {
-    return (
-        <div>
+function Home() {
+  return (
+    <div>
+      <img src="/images/home.png" alt="" className="homeImage"></img>
 
-    
-    
-    <img src = "/images/home.png" alt = "" className = "homeImage"></img>
+      <CardColumns>
+        <Card>
+          <Card.Img variant="top" src="/images/food.8jpeg.png" />
+          <Card.Body>
+            <Card.Title>Card title that wraps to a new line</Card.Title>
+            <Card.Text>
+              This is a longer card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </Card.Text>
+          </Card.Body>
+        </Card>
 
-    <CardColumns>
-    <Card>
-    <Card.Img variant="top" src="/images/food.8jpeg.png" />
-    <Card.Body>
-      <Card.Title>Card title that wraps to a new line</Card.Title>
-      <Card.Text>
-        This is a longer card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
-      </Card.Text>
-    </Card.Body>
-    </Card>
+        <Card>
+          <Card.Img variant="top" src="/images/123.jpeg" />
+          <Card.Body>
+            <Card.Title>Card title that wraps to a new line</Card.Title>
+            <Card.Text>
+              This is a longer card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </Card.Text>
+          </Card.Body>
+        </Card>
 
-    <Card>
-    <Card.Img variant="top" src="/images/123.jpeg" />
-    <Card.Body>
-      <Card.Title>Card title that wraps to a new line</Card.Title>
-      <Card.Text>
-        This is a longer card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
-      </Card.Text>
-    </Card.Body>
-    </Card>
+        <Card>
+          <Card.Img variant="top" src="/images/d1.jpeg" />
+          <Card.Body>
+            <Card.Title>Card title that wraps to a new line</Card.Title>
+            <Card.Text>
+              This is a longer card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </CardColumns>
 
-    <Card>
-    <Card.Img variant="top" src="/images/d1.jpeg" />
-    <Card.Body>
-      <Card.Title>Card title that wraps to a new line</Card.Title>
-      <Card.Text>
-        This is a longer card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
-      </Card.Text>
-    </Card.Body>
-    </Card>
-
-        
-    </CardColumns>
-
-    <h2 className = "aboutSection"> About us</h2>
-        <p>
-             meowmeow
-        </p> 
- 
-
-
-
-</div>
-
-            
-    )
-
+      <h2 className="aboutSection"> About us</h2>
+      <p>meowmeow</p>
+    </div>
+  );
 }
 
 export default Home;
-
-
-
-
