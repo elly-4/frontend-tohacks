c3af9d948125c98512cc5f8e5e237d8f71e8c16a
Elly Lastname <ellyy.yyu@gmail.com>
2021-05-08 20:35:00 -0400

    pog

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import Signup from "./components/pages/Signup"
+import PickRecipes from "./components/pages/PickRecipes"
+import Home from "./components/pages/Home"
 
 
 export default function App() {
@@ -10,6 +12,8 @@ export default function App() {
      <Switch>  
       
      <Route path = '/signup' exact component = {Signup} />
+     <Route path = '/pickrecipes' exact component = {PickRecipes}/>
+     <Route path = '/home' exact component = {Home}/>
      </Switch>  
   
    </Router>
--- src/components/pages/Signup.js
+++ src/components/pages/Signup.js
@@ -9,14 +9,14 @@ function SignUp () {
             <form >
                 <h1 className = "signUp"> Sign up </h1>
                 <label className = "firstName"> First Name </label> <input type = "text" className = "enterValue"  placeholder = "First Name "/><br />
+                <label className = "phoneNumber"> Phone Number</label> <input type = "text"  className = "enterValue" placeholder = "Phone Number"/><br />
+                <label  className = "password"> Password</label> <input type = "text"  className = "enterValue" placeholder = "Password"/> <br />  
+                <label className = "confirmPassword"> Confirm Password</label> <input type = "text"  className = "enterValue" placeholder = "Confirm Password"/><br />
                 <label  className = "lastName"> Last Name</label> <input type = "text" className = "enterValue" placeholder = "Last Name"/><br />
 
                 <label className = "address"> Address</label> <input type = "text"  className = "enterValue" placeholder = "Address"/><br />
                 <label className = "postalCode"> Postal Code</label> <input type = "text"  className = "enterValue" placeholder = "Postal Code"/><br />
                 <label className = "emailAddress"> Email Address</label> <input type = "text"  className = "enterValue" placeholder = "Email Address"/><br />
-                <label className = "phoneNumber"> Phone Number</label> <input type = "text"  className = "enterValue" placeholder = "Phone Number"/><br />
-                <label  className = "password"> Password</label> <input type = "text"  className = "enterValue" placeholder = "Password"/> <br />  
-                <label className = "confirmPassword"> Confirm Password</label> <input type = "text"  className = "enterValue" placeholder = "Confirm Password"/><br />
                <input  className = "submitButton" type = "submit" value="Submit"/>
             </form>
 
